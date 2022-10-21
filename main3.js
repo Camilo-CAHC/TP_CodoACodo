23763f82b20923d0a1e08939542dce200aa04bf6
Camilo-CAHC <camilo1723@gmail.com>
2022-10-21 13:34:26 -0300

    Se agrega el JSON elaborado por Mannuel en rama-1

--- main3.js
+++
@@ -1,9 +0,0 @@
-/* navegarpie=`        
-<div class="item1" style="background-color: rgb(0, 0, 0)">
-    <nav id="pestañas">
-        <a class="pest" href="index.html">Nosotros</a>
-        <a class="pest" href="menu.html">Menu</a>
-        <a class="pest" href="pedidos.html">Pedidos</a>
-        <a class="pest" href="promociones.html">Promociones</a>
-    </nav>`
-document.write(navegarpie)
--- receta.js
+++ receta.js
@@ -1,23 +1,25 @@
-createApp({
-    data(){
-        return{
-            url:`https://www.themealdb.com/api/json/v1/1/search.php?f=a`,
-            meals:[]
+const {createApp}=Vue
 
+    createApp({
+        data(){
+            return{
+                url:`https://www.themealdb.com/api/json/v1/1/search.php?f=a`,
+                meals:[]
+
+            }
+        },
+        methods:{
+            fetchData(url){
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        this.meals=data.meals
+                        console.log(this.comida)
+                    })
+            }
+        },
+        created(){
+            this.fetchData(this.url)
         }
-    },
-    methods:{
-        fetchData(url){
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    this.meals=data.meals
-                    console.log(this.comida)
-                })
-        }
-    },
-    created(){
-        this.fetchData(this.url)
-    }
-})
-.mount(`#app`)
+    })
+    .mount(`#app`)
